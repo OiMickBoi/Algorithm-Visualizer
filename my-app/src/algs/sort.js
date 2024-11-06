22dcbcaef0e5f52c6d947b8f145577342b65ef2a
Joshua Boyle <oimickboi@gmail.com>
2024-11-06 14:13:56 -0700

    bubble copy

--- my-app/src/algs/sort.js
+++ my-app/src/algs/sort.js
@@ -12,3 +12,18 @@ export const bubbleSort = (array) => {
     }
     return array;
 }; 
+
+export const bubleSort2 = (array) => {
+    const size = array.length;
+    for (let i = 0; i < size-1; i++) {
+        for (let j = 0; j < size-1; j++) {
+            if (array[j] > array[j+1]) {
+                let temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp;
+            }
+        }
+        
+    }
+    return array;
+}; 
